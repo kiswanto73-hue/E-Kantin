f06750d07b54a1fe78642a94e2f3580916ad81ff
kiswanto73-hue <kiswanto73@guru.sd.belajar.id>
2026-01-30 20:59:05 +0700

    Update sw.js
    okay

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = 'ekantin-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './kasir.html' // Tambahkan nama file kasir bapak di sini jika namanya berbeda
+];
 
+// Proses Instalasi Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Logika agar aplikasi tetap bisa dibuka meski sinyal lemah
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
